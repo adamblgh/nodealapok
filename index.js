afbef267965297409e039f6fd9dc99713db86c37
Your Name <you@example.com>
2022-09-15 13:11:00 +0200

    'csutibackendapi'

--- index.js
+++ index.js
@@ -15,4 +15,34 @@ app.get('/',(request,response)=>{
     })
 })
 
+app.get('/author/:author',(request,response)=>{
+    const {author} = request.params
+    db.query('SELECT title from books where author=? ORDER BY title;',[author],(err,result)=>{
+        if(err)
+            console.log(err)
+        else    
+            response.send(result)
+    })
+})
+
+app.get('/kat/:kat/ev/:ev',(request,response)=>{
+    const {kat,ev} = request.params
+    db.query('SELECT * from books where year>=? and category like ?',[ev,kat],(err,result)=>{
+        if(err)
+            console.log(err)
+        else    
+            response.send(result)
+    })
+})
+
+app.get('/id/:id',(request,response)=>{
+    const {id} = request.params
+    db.query('SELECT * FROM books where id=?',[id],(err,result)=>{
+        if(err)
+            console.log(err)
+        else    
+            response.send(result)
+    })
+})
+
 app.listen(5000,()=>console.log('server on port 5000...'))
